ffc062807bfb7bc47e4fb20a08f6b987d0ef63fd
Hashib Raja <hashibraja23@gmail.com>
2023-10-05 09:18:21 +0530

    showing nav bar and logo in home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
