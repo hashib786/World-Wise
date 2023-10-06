f8affb613a1fd7c3118f7cbc3294eed051e13627
Hashib Raja <hashibraja23@gmail.com>
2023-10-06 09:10:28 +0530

    In app created side bar component

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,5 +1,12 @@
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+
 const AppLayout = () => {
-  return <div className="app">AppLayout</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default AppLayout;
