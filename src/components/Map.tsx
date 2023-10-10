adcdf8181322c9fe5093fe3ad5f4b9cddc275ed5
Hashib Raja <hashibraja23@gmail.com>
2023-10-10 16:46:58 +0530

    Downloaded leaflet and showing actual map 🗺️🗺️

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,24 +1,31 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Map = () => {
-  const navigate = useNavigate();
-  const [searchParam, setSearchParam] = useSearchParams();
-  const lat = searchParam.get("lat");
-  const lng = searchParam.get("lng");
+  // const navigate = useNavigate();
+  // const [searchParam, setSearchParam] = useSearchParams();
+  // const lat = searchParam.get("lat");
+  // const lng = searchParam.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Hello Hashib How are you ?</h1>
-      <h2>
-        Position : {lat} : {lng}
-      </h2>
-      <button
-        className="btn"
-        onClick={() => setSearchParam({ lat: "23", lng: "20" })}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
       >
-        Change Url
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
