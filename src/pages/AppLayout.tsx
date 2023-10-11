142a59c64895eb659b759a77c01a6d4728008669
Hashib Raja <hashibraja23@gmail.com>
2023-10-11 15:45:04 +0530

    when user login then that one is access app page otherwise navigate /login

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,18 +1,18 @@
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { useAuth } from "../contexts/authContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/authContext";
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import User from "../components/User";
 import styles from "./AppLayout.module.css";
 
 const AppLayout = () => {
-  // const { isAuthenticated } = useAuth();
-  // const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   navigate("/login", { replace: true });
-  // }, [isAuthenticated, navigate]);
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/login", { replace: true });
+  }, [isAuthenticated, navigate]);
 
   return (
     <div className={styles.app}>
