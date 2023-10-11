41f102e6ce089b8897fdfd6dbf2d325e28c75f8a
Hashib Raja <hashibraja23@gmail.com>
2023-10-11 15:02:18 +0530

    created auth context

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,73 @@
+import { ReactNode, createContext, useReducer } from "react";
+
+interface UserI {
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+}
+
+interface authI {
+  user: UserI | null;
+  isAuthenticated: boolean;
+  login: (user: string, password: string) => void;
+  logout: () => void;
+}
+
+interface intialStateI {
+  user: null | UserI;
+  isAuthenticated: boolean;
+}
+
+const intialState: intialStateI = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const AuthContext = createContext<authI>({
+  ...intialState,
+  login: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  logout: function (): void {
+    throw new Error("Function not implemented.");
+  },
+});
+
+const FAKE_USER: UserI = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+type ActionType = { type: "login"; payload: UserI } | { type: "logout" };
+
+const reducer = (state: intialStateI, action: ActionType): intialStateI => {
+  const { type } = action;
+  switch (type) {
+    case "login":
+      return { ...state, isAuthenticated: true, user: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
+
+  const login = (user: string, password: string) => {
+    console.log(user, password);
+  };
+
+  const logout = () => {};
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
